2d45052776bc4d0b8e9ceef9fdefeda610a1ab4e
Xyzonox <85287832+Xyzonox@users.noreply.github.com>
2024-04-16 12:58:48 -0700

    Update VectorCellConvolution.py

--- CellAuto/VectorCellConvolution.py
+++ CellAuto/VectorCellConvolution.py
@@ -2,23 +2,27 @@ import taichi as ti
 import taichi.math as math
 import random
 
-ti.init(arch=ti.cpu)
-
 #Parameters
+ti.init(arch=ti.gpu)
 n=800 # resolution
-kernelfac = 1.2 #Multiple for convolution kernel, Turn this up if cells are vanishing, down if blowing up / flickering
-cellVision = 3 #How many cells a cell can see in all directions around it
-
+kernelfac = 1 #Multiple for convolution kernel, Turn this up if cells are vanishing, down if blowing up / flickering
+cellVision = 2 #How many cells a cell can see in all directions around it
 
 filterSize = 2*cellVision+1
 filterKernel = ti.Vector.field(3,dtype=ti.f32, shape=(filterSize,filterSize))
-
+filterRandom = ti.Vector.field(3,dtype=ti.f32, shape=(filterSize,filterSize))
 cells = ti.Vector.field(3,dtype=ti.f32, shape=(n,n)) #Stores values per pixel to be displayed
-pixels = ti.Vector.field(3,dtype=ti.f32, shape=(n,n))
+pixels = ti.Vector.field(3,dtype=ti.f32, shape=(n,n)) #Stores values per cell 
 prev = ti.Vector.field(3,dtype=ti.f32, shape=(n,n)) #Stores values per pixeled for last iteration
 convolv = ti.Vector.field(3,dtype=ti.f32, shape=(n,n)) #Stores convolution data for every pixel
-accum = ti.Vector.field(3,dtype=ti.f32, shape=(n,n))
+accum = ti.Vector.field(3,dtype=ti.f32, shape=(n,n)) #Stores values per pixel to be displayed, For Blur
  
+#Non deterministic number generation
+for i in range(filterSize):
+    for j in range(filterSize):
+        for k in range(3):
+            filterRandom[i,j][k] = (2*random.random())-1
+
 @ti.func
 def rand():
     return ti.random(ti.f32)
@@ -53,7 +57,7 @@ def Squiggle(x: float):
     elif x < -1:
         result = 1+x
     elif x <= 0:
-        result = 0
+        result = x + -.9*x
     return clamp(result)
 @ti.func
 def clamp(x: float):
@@ -75,13 +79,66 @@ def Maxim(x):
 def Average(x):
     return (x[0] + x[1] + x[2])/3
 
+#Kernel Matrix Values
+@ti.func
+def Pattern1(d, e, filterSize, kernelfac):
+    row = d
+    row = (row - (filterSize-1)/2)
+    #if row < 0:
+        #row = row * -1
+
+    col = e
+    col = (col - (filterSize-1)/2)
+    #if col < 0:
+        #col = col * -1
+
+    result = kernelfac * ((row + col - 1))
+    return result
+@ti.func
+def Pattern2(d, e, filterSize, kernelfac):
+    row = d
+    row = (row - (filterSize-1)/2)
+    if row < 0:
+        row = row * -1
+    row = row - (filterSize-1)/2
+    if row < 0:
+        row = row * -1
+    #row = row/2
+
+    col = e
+    col = (col - (filterSize-1)/2)
+    if col < 0:
+        col = col * -1
+    col = col - (filterSize-1)/2
+    if col < 0:
+        col = col * -1
+    result = kernelfac * ((row + col - 1))
+    return result
+
+@ti.func
+def Pattern3(d, e, filterSize, kernelfac):
+    row = d
+    row = (row - (filterSize-1)/2)
+
+    col = e
+    col = (col - (filterSize-1)/2) 
+    if col > 0:
+        col = row
+    if row > 0:
+        row = col
+
+    result = ((row + col + 1))
+    return result
+
 @ti.kernel
 def setup():
-    for d,e in filterKernel:
-        for k in range(3):
-            filterKernel[d,e][k] = ((rand()*2)-1) * kernelfac #Kernel Matrix Generation
+    for d,e in filterKernel: #Kernel Matrix Generation
+        for k in range(3): 
+            #filterKernel[d,e][k] = ((rand()*2)-1) * kernelfac #Random Matrix
+            filterKernel[d,e][k] = Pattern3(d,e,filterSize,kernelfac) + filterRandom[i,j][k] * kernelfac 
     for i, j in prev: #parallized over pixels
-        prev[i,j] = ti.Vector([rand(), rand(), rand()])
+        #prev[i,j] = ti.Vector([rand(), rand(), rand()]) # white noise
+        prev[i,j] = clamp((i % -50)+5) + clamp((j % -50)+5) #Grid
 
 @ti.kernel
 def CellAuto():
@@ -95,7 +152,7 @@ def CellAuto():
                 convolv[i,j][k] += Average(prev[row,col]) * filterKernel[dx,dy][k]  
         
         for k in range(3):
-            cells[i,j][k] = RevGauss(convolv[i,j][k])  
+            cells[i,j][k] = Squiggle(convolv[i,j][k])  
 
 @ti.kernel
 def paint():
@@ -112,7 +169,9 @@ def paint():
 
 
 gui = ti.GUI("Cell Auto", res=(n,n))
+
 setup()
+print(filterKernel)
 gui.set_image(prev)
 gui.show()
 while gui.running:
